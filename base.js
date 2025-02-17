32532ff0c8106c31ea83d7ece15120065c119c38
sofieutke <sobo0001@stud.kea.dk>
2025-02-17 17:46:39 +0100

    feature-header done

--- base.js
+++ base.js
@@ -3,24 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
   // finder elementet og gemmer i variabel
   const burger = document.querySelector(".burger");
   const overlay = document.getElementById("myNav");
+  const closeBtn = document.querySelector(".closebtn");
 
   // tilføjer klik
   burger.addEventListener("click", function () {
     // skifter class til change
     burger.classList.toggle("change");
-
-    // Tjekker om bredde er 100%
-    if (overlay.style.width === "100%") {
-      // skjuler overlay
-      overlay.style.width = "0";
-    } else {
-      // viser overlay
-      overlay.style.width = "100%";
-    }
+    overlay.style.width = overlay.style.width === "100%" ? "0" : "100%";
   });
 
-  // lytter til klik til lukkeknappen
-  document.querySelector(".closebtn").addEventListener("click", function () {
+  closeBtn.addEventListener("click", function () {
     overlay.style.width = "0";
+    burger.classList.remove("change"); // Fjern farveændring, når menuen lukkes
   });
 });
